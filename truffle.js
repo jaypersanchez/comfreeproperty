49c029c02d6dce74147c9a12f131416fed9c0f2e
jaypersanchez <jaypersanchez@gmail.com>
2019-01-30 14:10:01 -0800

    additional changes

--- truffle.js
+++ truffle.js
@@ -1,4 +1,11 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+//var mnemonic = "parrot guide ribbon seed lady easy dizzy genre vote praise famous help";
+var mnemonic = "flee sadness churn mixture harbor hurry helmet grid valve frame seat voice";
 module.exports = {
+  rpc: {
+    host: "localhost",
+    port: '8545'
+  },
   networks: {
     ganache_cli: {
       host: "localhost",
@@ -15,6 +22,12 @@ module.exports = {
       port: 7545,
       network_id: "*",
       from: "0xB3E68458CF37ee946eABF8EbFFd8fb78811A202e"
+    },
+    rinkeby: {
+      provider: new HDWalletProvider(mnemonic,"https://rinkeby.infura.io/v3/051f0a087fcb48ce8371eab1491f7e93"),
+      network_id: 4,
+      gas: 6995427  
+      
     }
   }
 };
